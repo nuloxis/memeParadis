a297f9a8b29ad0d8002e1e93737acc09ff2a74e8
LibaiK <97013414+LibaiK@users.noreply.github.com>
2023-03-14 18:12:29 +0100

    Próbálkozas
    1

--- back/memeparadisBack/src/main/java/com/mycompany/memeparadisback/Configuration/Database.java
+++ back/memeparadisBack/src/main/java/com/mycompany/memeparadisback/Configuration/Database.java
@@ -9,7 +9,7 @@ package com.mycompany.memeparadisback.Configuration;
  * @author krist
  */
 public class Database {
-     private static final String persistenceUnitName = "com.mycompany_memeparadisBack-war-0.1-SNAPSHOTPU";
+     private static final String persistenceUnitName = "my_persistence_unit";
     public static String  getPuName(){
         return Database.persistenceUnitName;
     }
