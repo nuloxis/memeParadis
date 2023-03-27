24afea9d39d2dc16e20fd202db9ce6fb8b8f63e7
nuloxis <102282562+nuloxis@users.noreply.github.com>
2023-03-27 16:52:51 +0200

    loginguard

--- front/memeparadis/src/app/_guard/auth.service.ts
+++ front/memeparadis/src/app/_guard/auth.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Route, Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,8 @@ import { Injectable } from '@angular/core';
 export class AuthService {
 
   constructor() { }
+
+  IsLoggedIn(){
+    return localStorage.getItem("name")
+  }
 }
--- front/memeparadis/src/app/app-routing.module.ts
+++ front/memeparadis/src/app/app-routing.module.ts
@@ -3,13 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './_components/login/login.component';
 import { MainPageComponent } from './_components/main-page/main-page.component';
 import { RegistrationComponent } from './_components/registration/registration.component';
+import { AuthGuard } from './_guard/auth.guard';
 
 
 const routes: Routes = [
   {path:'login',component:LoginComponent},
   {path:'',component:LoginComponent},
   {path:'registration',component:RegistrationComponent},
-  {path:'main', component:MainPageComponent}
+  {path:'main', component:MainPageComponent, canActivate:[AuthGuard]}
 ];
 
 @NgModule({
