16e92585da01481d89f44e84d4f47d254e9fc676
nuloxis <102282562+nuloxis@users.noreply.github.com>
2023-04-03 12:06:27 +0200

    login_registration_page_Style

--- databases/memeparadis.sql
+++ databases/memeparadis.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2023. Már 20. 09:28
+-- Létrehozás ideje: 2023. Már 29. 13:23
 -- Kiszolgáló verziója: 10.4.24-MariaDB
 -- PHP verzió: 7.4.29
 
@@ -28,8 +28,11 @@ DELIMITER $$
 -- Eljárások
 --
 DROP PROCEDURE IF EXISTS `addNewUser`$$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `addNewUser` (IN `nameIN` VARCHAR(200), IN `emailIN` VARCHAR(200), IN `passwordIN` TEXT, IN `birth_dateIN` DATE)   INSERT INTO `user` (`user`.`name`,`user`.`email`,`user`.`password`,`user`.`birth_date`)
+CREATE DEFINER=`root`@`localhost` PROCEDURE `addNewUser` (IN `nameIN` VARCHAR(200) CHARSET utf8mb4, IN `emailIN` VARCHAR(200) CHARSET utf8mb4, IN `passwordIN` TEXT CHARSET utf8mb4, IN `birth_dateIN` DATE)   INSERT INTO `user` (`user`.`name`,`user`.`email`,`user`.`password`,`user`.`birth_date`)
 VALUES(nameIN,emailIN,passwordIN,birth_dateIN)$$
+
+DROP PROCEDURE IF EXISTS `checkEmailUnique`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `checkEmailUnique` (OUT `result` INT(10), IN `emailIN` VARCHAR(100) CHARSET utf8)   SELECT COUNT(`user`.`id`) into result FROM `user` WHERE `user`.`email` = emailIN$$
 
 DROP PROCEDURE IF EXISTS `createcomment`$$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `createcomment` (IN `commentIN` VARCHAR(100))   INSERT INTO `comment` (`comment`.`comment`) VALUES (commentIN)$$
@@ -53,6 +56,18 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `deleteTag` (IN `idIN` INT)   DELETE
 DROP PROCEDURE IF EXISTS `deleteUser`$$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `deleteUser` (IN `idIN` INT)   DELETE FROM `user` WHERE `user`.`id`=idIN$$
 
+DROP PROCEDURE IF EXISTS `GetMostLikedPosts`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `GetMostLikedPosts` ()   BEGIN
+SELECT `content`.`id`,`content`.`likes` FROM content
+ORDER BY `likes` DESC;
+END$$
+
+DROP PROCEDURE IF EXISTS `getUserByID`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `getUserByID` (IN `idIN` INT)   SELECT * FROM user WHERE user.id = idIN$$
+
+DROP PROCEDURE IF EXISTS `login`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `login` (IN `emailIN` VARCHAR(200), IN `pwIN` VARCHAR(200))   SELECT * FROM `user` WHERE `user`.`email` = emailIN AND `user`.`password` = pwIN$$
+
 DROP PROCEDURE IF EXISTS `updateComment`$$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `updateComment` (IN `commentIN` VARCHAR(100), IN `idIN` INT)   UPDATE `comment` SET `comment`.`comment` = commentIN WHERE `comment`.`id` = idIN$$
 
@@ -74,13 +89,11 @@ DELIMITER ;
 DROP TABLE IF EXISTS `comment`;
 CREATE TABLE IF NOT EXISTS `comment` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `comment` varchar(100) NOT NULL,
-  `userId` int(11) NOT NULL,
-  `contentId` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `content` varchar(100) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `userId` (`userId`),
-  KEY `contentId` (`contentId`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -109,7 +122,7 @@ INSERT INTO `content` (`id`, `adult_content`, `uploader_name`, `language`, `like
 (1, 0, 2, 'hun', 0, 0, ''),
 (2, 1, NULL, 'hun', 0, 0, ''),
 (3, 1, NULL, 'awad', 0, 0, ''),
-(4, 1, 1, 'FR', 0, 0, '');
+(4, 1, NULL, 'FR', 0, 0, '');
 
 -- --------------------------------------------------------
 
@@ -135,7 +148,7 @@ CREATE TABLE IF NOT EXISTS `content_tag` (
 
 DROP TABLE IF EXISTS `password_reset`;
 CREATE TABLE IF NOT EXISTS `password_reset` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `user_id` int(11) DEFAULT NULL,
   `token` varchar(100) NOT NULL,
   `token_expire` date NOT NULL,
@@ -153,7 +166,7 @@ CREATE TABLE IF NOT EXISTS `password_reset` (
 
 DROP TABLE IF EXISTS `tags`;
 CREATE TABLE IF NOT EXISTS `tags` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `tag` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -170,46 +183,39 @@ CREATE TABLE IF NOT EXISTS `user` (
   `name` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
-  `registration_date` datetime NOT NULL DEFAULT current_timestamp(),
+  `registration_date` timestamp NOT NULL DEFAULT current_timestamp(),
   `birth_date` date DEFAULT NULL,
   `access_type` tinyint(1) NOT NULL DEFAULT 0,
   `is_deleted` tinyint(1) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
 
 --
 -- A tábla adatainak kiíratása `user`
 --
 
 INSERT INTO `user` (`id`, `name`, `email`, `password`, `registration_date`, `birth_date`, `access_type`, `is_deleted`) VALUES
-(1, 'lkj', 'krcs@gmail.com', '4551', '2023-02-20 15:50:37', '2023-02-16', 0, 0),
-(2, 'mjhh', 'jjhh@gmail.com', '665485', '2023-02-20 15:57:59', '2023-02-15', 0, 0),
-(3, 'oulé', 'klélk@gmail.com', '5766', '2023-02-20 15:59:02', '2023-02-09', 1, 0),
-(4, 'teszt', 'tesz@gmail.com', '54687', '2023-02-21 09:48:59', '2023-12-11', 0, 0);
+(2, 'mjhh', 'jjhh@gmail.com', '665485', '2023-02-20 14:57:59', '2023-02-15', 0, 0),
+(3, 'oulé', 'klélk@gmail.com', '5766', '2023-02-20 14:59:02', '2023-02-09', 1, 0),
+(4, 'teszt', 'tesz@gmail.com', '54687', '2023-02-21 08:48:59', '2023-12-11', 0, 0),
+(5, 'asdasasd', 'ghfgh@fgzuhfgu.com', 'asd123$R', '2023-03-20 15:48:15', '2001-02-03', 0, 0),
+(6, 'asasad', 'valami@valami.hu', 'asd123$R', '2023-03-20 15:48:42', '2001-02-03', 0, 0),
+(7, 'ASdasddddd', 'YYYafasf@fgzuhfgu.com', 'asd123$R', '2023-03-20 15:57:15', '2001-02-03', 0, 0),
+(8, 'asdasdasd', 'aSasdas@gmail.hu', 'AScasas!221', '2023-03-20 16:02:43', '2023-03-15', 0, 0),
+(10, 'kléklklélkékl', 'AAAAa@fgzuhfgu.com', 'asd123$R', '2023-03-23 18:35:55', '2001-02-03', 0, 0),
+(11, 'Teszt0011', 'Teszttel1@gmtesz.com', 'AAwwee455_4', '2023-03-24 12:35:58', '2023-02-10', 0, 0),
+(12, 'Proabaaaa', 'asddsA@gahjj.com', 'AAA222fff__', '2023-03-26 12:18:28', '2023-03-16', 0, 0),
+(13, 'kléklklélkékl', 'teszta@fgzuhfgu.com', 'asd123$R', '2023-03-27 13:21:53', '2001-02-03', 0, 0);
 
 --
 -- Megkötések a kiírt táblákhoz
 --
 
 --
--- Megkötések a táblához `comment`
---
-ALTER TABLE `comment`
-  ADD CONSTRAINT `commentContentId_contentId` FOREIGN KEY (`contentId`) REFERENCES `content` (`id`),
-  ADD CONSTRAINT `commentUserId_userId` FOREIGN KEY (`userId`) REFERENCES `user` (`id`);
-
---
 -- Megkötések a táblához `content`
 --
 ALTER TABLE `content`
   ADD CONSTRAINT `userId_contentUploaderId` FOREIGN KEY (`uploader_name`) REFERENCES `user` (`id`) ON DELETE SET NULL ON UPDATE SET NULL;
-
---
--- Megkötések a táblához `content_tag`
---
-ALTER TABLE `content_tag`
-  ADD CONSTRAINT `contentID_contentTagContentId` FOREIGN KEY (`content_id`) REFERENCES `content` (`id`) ON DELETE SET NULL ON UPDATE SET NULL,
-  ADD CONSTRAINT `tagsId_contentTagContentTagsId` FOREIGN KEY (`tags_id`) REFERENCES `tags` (`id`) ON DELETE SET NULL ON UPDATE SET NULL;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
