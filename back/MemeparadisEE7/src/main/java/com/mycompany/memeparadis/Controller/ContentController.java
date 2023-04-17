e2516a9a78be7d7e74cb22a5d0d125967a64d387
LibaiK <97013414+LibaiK@users.noreply.github.com>
2023-04-17 15:41:38 +0200

    Ennyit nem is módosítottam
    Ha tudsz néz rá hogy mi kéne még bele mert megakadtam

--- back/MemeparadisEE7/src/main/java/com/mycompany/memeparadis/Controller/ContentController.java
+++ back/MemeparadisEE7/src/main/java/com/mycompany/memeparadis/Controller/ContentController.java
@@ -13,6 +13,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -37,8 +38,18 @@ public class ContentController {
     @Consumes(MediaType.APPLICATION_JSON)
     public void putJson(String content) {
     }
+    
+    @POST
+     @Path("createContent")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response createContent(Content content){
+//        String result = conts.createContent(content);
+        return null;
+    }
+    
     @GET
     @Produces(MediaType.APPLICATION_JSON)
+    @Path("getAllContent")
     public Response getAllContent() {
         List<Content> resultList;
         try {
