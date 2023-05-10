6a010b17899dc8e72f2bb29524f5e1f083d2f8ae
nuloxis <102282562+nuloxis@users.noreply.github.com>
2023-05-10 19:21:16 +0200

    registrationVizsgalat

--- front/memeparadis/src/app/_components/registration/registration.component.ts
+++ front/memeparadis/src/app/_components/registration/registration.component.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { User } from 'src/app/models/user';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
@@ -20,11 +22,32 @@ export class RegistrationComponent implements OnInit {
     this.router.navigate([`${pageName}`]);
   }
 
-  onProductCreate(addNewUser: any){
-    console.log(addNewUser);
-    this.http.post('http://127.0.0.1:8080/MemeparadisEE7-1.0-SNAPSHOT/resources/User/addNewUser',addNewUser).subscribe((res)=>{
-      console.log(res);
-    });
+  onProductCreate(addNewUser: User){
+    const birth :Date=new Date(addNewUser.birthDate);
+    const now=new Date();
+    const age=now.getFullYear() - birth.getFullYear();
+    const myString=addNewUser.password
+    const containsUppercase = /[A-Z]/.test(myString);
+    const containsNumber = /\d/.test(myString);
+    const containsSpecialChar = /[@$!%*?&]/.test(myString);
+
+    if(age>=18){
+      this.http.post('http://127.0.0.1:8080/MemeparadisEE7-1.0-SNAPSHOT/resources/User/addNewUser',addNewUser,{responseType:'text'}).subscribe((res)=>{
+        console.log(res);
+        if(addNewUser.name.length<5){
+          alert("User name must be at least 5 characters!")
+        }
+        else if(containsUppercase==false||containsNumber==false||containsSpecialChar==false){
+          alert("awdawdaw")
+        }
+        else if(res="Email is already taken!"){
+          alert("Email is already taken!")
+        }
+      });
+    }else{
+      alert("birht not correct!")
+    }
+
   }
 
 
